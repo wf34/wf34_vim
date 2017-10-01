2e482b775130b490aa4cee1b026192d3d54ebfd7
wf34 <wf34@users.noreply.github.com>
2017-10-01 21:24:28 +0300

    [add] portage update code

--- system/portage_updater.sh
+++ system/portage_updater.sh
@@ -1,6 +1,53 @@
 #!/usr/bin/env bash
 
-set -eu
+set -u
 
-touch /tmp/timer_log.txt
-echo 'hyi' > /tmp/timer_log.txt
+function write_mail {
+  cat ${log} | mailx -s 'tpad weekly system update' user@server.com
+}
+
+log=/var/log/portage_updater.log
+rm ${log}
+touch ${log}
+
+ping ya.ru -c 1 -W 3 &> /dev/null
+ping_status=$?
+
+echo 'Ping status: '${ping_status} | tee -a ${log}
+if [ ${ping_status} -ne 0 ]; then
+  exit 1
+fi
+
+eix-sync 2>&1 | tee -a ${log}
+layman -S 2>&1 | tee -a ${log}
+
+echo '=== systemd emerge === ' | tee -a ${log}
+sudo emerge --update --deep --newuse --tree --quiet \
+            sys-apps/systemd 2>&1 | tee -a ${log}
+
+echo '=== world emerge === ' | tee -a ${log}
+sudo emerge --update --deep --newuse --tree --quiet \
+            @world 2>&1 | tee -a ${log}
+
+if [ $? -eq 0 ]; then
+  echo '=== @preserved-rebuild === ' | tee -a ${log}
+  emerge --quiet @preserved-rebuild 2>&1 | tee -a ${log}
+
+  echo '=== revdep-rebuild === ' | tee -a ${log}
+  revdep-rebuild -v 2>&1 | tee -a ${log}
+  
+  perl-cleaner --all 2>&1 | tee -a ${log}
+  echo '=== depclean === ' | tee -a ${log}
+  emerge --depclean --quiet 2>&1 | tee -a ${log}
+  glsa-check -t all 2>&1 | tee -a ${log}
+  glsa-check -f all 2>&1 | tee -a ${log}
+  eclean-dist -d
+
+  echo '=== eix-test-obsolete === ' | tee -a ${log}
+  eix-test-obsolete
+else
+  echo 'emerge failed, skip post-emerge steps' | tee -a ${log}
+fi
+
+echo '=== finish === ' | tee -a ${log}
+write_mail
